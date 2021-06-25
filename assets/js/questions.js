5efe72e6de4a7754320deb1274ff2c09f037c876
Georgette-Lumbe <lumbe.georgette@gmail.com>
2021-06-25 23:31:25 +0000

    Add some questions in the questions js file

---
+++ assets/js/questions.js
@@ -0,0 +1,30 @@
+
+////Creating an array and passing the number, questions, options, and answers
+let quiz = [
+    {
+        question: 'Who was elected President of the United States in 2017?',
+        options: ['Donald Trump', 'Barack Obama', 'George Bush'],
+        answer: 0
+    },
+    {
+        question: 'What is the national language of Canada?',
+        options: ['English','Dutch','French'],
+        answer:1
+    },
+    {
+        question: 'A la Crecy is a French dish made of what?',
+        options: ['Apples', 'Carrots', 'Potatoes'],
+        answer:0
+    },
+    {
+        question: '70 - 33 = 103?',
+        options: ['true', 'false'],
+        answer:2
+    },
+    {
+        question: 'Which core ingredient is important to cook a savory dish?',
+        options: ['Salt', 'Butter', 'Sugar'],
+        answer:3
+    },
+
+]
