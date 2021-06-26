b5764a368f11cd35874870cd437be235cb3b766f
Georgette-Lumbe <lumbe.georgette@gmail.com>
2021-06-26 00:04:24 +0000

    set question text and get random question

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,35 @@ const optionList =  document.querySelector('.optionList');
 
 let questionCounter = 0;
 let currentQuestion;
-let availableQuestions = [];
+let accessibleQuestions = [];
 
 // push the questions into accessibleQuestions Array
 function setAccessibleQuestions(){
+    const totalQuestion = quiz.length;
+    for(let i = 0; i < totalQuestion; i++){
+        accessibleQuestions.push(quiz[i])
+    }
+}
+
+// set question number, question and options
+function getNewQuestion(){
+
+    // set question number
+    questionNumber.innerHTML = "Question " + (questionCounter + 1) + " of " + quiz.length;
+
+    // set question text and get random question
+    const questionIndex = accessibleQuestions[Math.floor(Math.random() * accessibleQuestions.length)]
+    currentQuestion = questionIndex;
+    questionText.innerHTML = currentQuestion.question;
+   // console.log(questionIndex)
 
 }
 
 window.onload = function(){
 
-    setAccessibleQuestions()()
+    // set all questions in accessibleQuestions array first
+    setAccessibleQuestions();
+
+    // call getNewQuestion second
+    getNewQuestion();
 }
