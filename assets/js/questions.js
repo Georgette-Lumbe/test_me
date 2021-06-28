3ac044a46a50b259d04d0c336c97c498962c5b75
Georgette-Lumbe <lumbe.georgette@gmail.com>
2021-06-28 19:05:01 +0000

    Add more questions in the questions.js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -13,7 +13,7 @@ let quiz = [{
 {
     question: 'A la Crecy is a French dish made of what?',
     options: ['Apples', 'Carrots', 'Potatoes'],
-    answer: 3
+    answer: 1
 },
 {
     question: '70 - 33 = 103?',
@@ -23,12 +23,12 @@ let quiz = [{
 {
     question: 'Which core ingredient is important to cook a savory dish?',
     options: ['Salt', 'Butter', 'Sugar'],
-    answer: 2
+    answer: 0
 },
 {
     question: 'Saudi Arabia is the biggest producer of?',
     options: ['Coffee', 'Coal', 'Oil'],
-    answer: 0
+    answer: 2
 },
 {
     question: 'What native country is Brazil?',
@@ -73,5 +73,85 @@ let quiz = [{
     question: 'What would you pour on top of an Irish coffee?',
     options: ['Cream', 'Chocolate', 'Vanilla Extract'],
     answer: 0
+},
+{
+    question: 'When did Jonas Brothers make their comeback to the music world?',
+    options: ['2015', '2019', '2011'],
+    answer: 1
+},
+{
+    question: ' What is the national animal of Pakistan?',
+    options: ['Loin', 'Peacock', 'Markhor'],
+    answer: 2
+},
+{
+    question: 'Which religion has a God specified for each Rain, Money, Children, and Love?',
+    options: ['Hinduism', 'Islam', 'Buddism'],
+    answer: 0
+},
+{
+    question: 'How many colors in the Rainbow?',
+    options: ['One', 'Five', 'Seven'],
+    answer: 2
+},
+{
+    question: 'What does independence mean?',
+    options: ['Permission','Goodwill', 'Freedom'],
+    answer: 1
+},
+{
+    question: 'What is a tomato?',
+    options: ['Fruit', 'Vegetables', 'Herb'],
+    answer: 0
+},
+{
+    question: 'What is the capital of Democratic Republic of Congo?',
+    options: ['Brazzaville', 'Kinshasa', 'Abidjan'],
+    answer: 1
+},
+{
+    question: 'Ancient Egyptian Houses were built of what?',
+    options: ['Mud', 'Wood', 'Brick'],
+    answer: 1
+},
+{
+    question: 'What fabric is made of flax?',
+    options: ['Cotton', 'Linen', 'Lawn'],
+    answer: 0
+},
+{
+    question: 'What is the average weight of the human brain?',
+    options: ['14 kilos', '4 kilos', '14 grams'],
+    answer: 1
+},
+{
+    question: 'Who was the first person to create pizza',
+    options: ['Raffaele Esposito', 'Nada Esposito', 'Conrado Esposito'],
+    answer: 0
+},
+{
+    question: 'Which country is infamously known as Arch Rival of Pakistan?',
+    options: ['Afghanistan', 'America', 'India'],
+    answer: 2
+},
+{
+    question: 'Which is ingredient found in traditional garam masala spice?',
+    options: ['Salt', 'Red Chili Powder', 'Turmeric'],
+    answer: 2
+},
+{
+    question: 'What is the currency of Qatar?',
+    options: ['Pound', 'Riyal', 'Euro'],
+    answer: 1
+},
+{
+    question: 'Which is the most common drink in Europe?',
+    options: ['Beer', 'Water', 'Drink'],
+    answer: 0
+},
+{
+    question: 'Which two colors mix to form pink?',
+    options: ['Grey and red', 'White and orange', 'White and Red'],
+    answer: 2
 },
 ]
